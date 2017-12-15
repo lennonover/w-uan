4db9097aa77fb23406f88c067b6cb48577b1c2b8
lennonover <lennonover@163.com>
2017-12-15 22:29:33 +0800

    updata changeLetter

--- js/uan.js
+++ js/uan.js
@@ -48,6 +48,7 @@ let uan = {
         3：大小写转换
         4：全部大写
         5：全部小写
+        6: 大写每个单词的首字母
      */
     changeLetter (str, type) {
         function ToggleCase(str) {
@@ -66,19 +67,17 @@ let uan = {
 
         switch (type) {
             case 1:
-                return str.replace(/\b\w+\b/g, function (word) {
-                    return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
-                });
+                return str.replace(/\b\w+\b/g, word  => word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase());
             case 2:
-                return str.replace(/\b\w+\b/g, function (word) {
-                    return word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase();
-                });
+                return str.replace(/\b\w+\b/g, word  => word.substring(0, 1).toLowerCase() + word.substring(1).toUpperCase());
             case 3:
                 return ToggleCase(str);
             case 4:
                 return str.toUpperCase();
             case 5:
                 return str.toLowerCase();
+            case 6:
+                return str.replace(/\b[a-z]/g, char => char.toUpperCase());
             default:
                 return str;
         }
